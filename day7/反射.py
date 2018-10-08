b951838caefaa0687cfcee9cc070d5e42aaae330
shuqian2017 <shuqian2017@gmail.com>
2018-10-08 23:42:26 +0800

    更新笔记

---
+++ day7/反射.py
@@ -0,0 +1,22 @@
+# -*-coding:utf-8 -*-
+"""
+@project: code
+@author: fke
+@file: 反射.py
+@time: 2018-09-27 01:26:
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG。   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
+
